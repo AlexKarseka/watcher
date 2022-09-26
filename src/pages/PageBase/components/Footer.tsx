26a4f27228578b254d8aca11c6a8afe2124ca537
AlexKarseka <alexkarseko@gmail.com>
2022-09-26 17:53:05 +0300

    add footer and footer styles

--- src/pages/PageBase/components/Footer.tsx
+++ src/pages/PageBase/components/Footer.tsx
@@ -1,19 +1,38 @@
 import React from "react";
 import css from './styles.module.css';
 
+import GitLogo from '../assets/gitHub.jpg';
+import API from '../assets/API.svg';
+
 const Footer = () => {
     return (
         <div className={css.footer}>
-            <div>Watcher</div>
+            <div className={css.logo}>
+                <a href="/">Watcher</a>
+            </div>
             <div>
-                <ul className={css.footer_first_list}>
-                    <li>Фильмы</li>
-                    <li>Сериалы</li>
-                    <li>Поиск</li>
+                <ul className={css.menu_list}>
+                    <li className={css.menu_list_item}>
+                        <a href="/movies">Movies</a>
+                    </li>
+                    <li className={css.menu_list_item}>
+                        <a href="/serials">Serials</a>
+                    </li>
+                    <li className={css.menu_list_item}>
+                        <a href="/search">Search</a>
+                    </li>
                 </ul>
-                <ul className={css.footer_second_list}>
-                    <li>Git</li>
-                    <li>API</li>
+                <ul className={css.footer_dev_list}>
+                    <li className={css.dev_list_item}>
+                        <a href="https://github.com/AlexKarseka/watcher">
+                            <img src={GitLogo} alt="gitLogo" height={100} width={100} />
+                        </a>
+                    </li>
+                    <li className={css.dev_list_item}>
+                        <a href="https://developers.themoviedb.org/3/getting-started/introduction">
+                            <img src={API} alt="APILogo" height={100} width={100} />
+                        </a>
+                    </li>
                 </ul>
             </div>
             <div>
--- src/pages/PageBase/components/Header.tsx
+++ src/pages/PageBase/components/Header.tsx
@@ -4,26 +4,26 @@ import css from "./styles.module.css";
 const Header = () => {
     return (
         <div className={css.header}>
-            <ul className={css.header_list}>
-                <li className={css.header_logo}>
+            <ul className={css.menu_list}>
+                <li className={css.logo}>
                     <a href="/">Watcher</a>
                 </li>
-                <li className={css.header_list_item}>
-                    <a href="/movies">Фильмы</a>
+                <li className={css.menu_list_item}>
+                    <a href="/movies">Movies</a>
                 </li>
-                <li className={css.header_list_item}>
-                    <a href="/serials">Сериалы</a>
+                <li className={css.menu_list_item}>
+                    <a href="/serials">Serials</a>
                 </li>
-                <li className={css.header_list_item}>
-                    <a href="/search">Поиск</a>
+                <li className={css.menu_list_item}>
+                    <a href="/search">Search</a>
                 </li>
             </ul>
             <div className={css.header_buttons}>
                 <a className={css.header_buttons_signin} href="/signin">
-                    Войти
+                   Log in
                 </a>
                 <a className={css.header_buttons_signup} href="/signup">
-                    Зарегистрироваться
+                    Sign up
                 </a>
             </div>
         </div>
