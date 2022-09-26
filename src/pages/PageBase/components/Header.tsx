3ef18504f36383fde3644f538ed7b910739f020a
AlexKarseka <alexkarseko@gmail.com>
2022-09-26 15:31:30 +0300

    add header and fonts

--- src/pages/PageBase/components/Header.tsx
+++ src/pages/PageBase/components/Header.tsx
@@ -5,18 +5,26 @@ const Header = () => {
     return (
         <div className={css.header}>
             <ul className={css.header_list}>
-                <li className={css.header_logo}>Watcher</li>
-                <li>Фильмы</li>
-                <li>Сериалы</li>
-                <li>Поиск</li>
+                <li className={css.header_logo}>
+                    <a href="/">Watcher</a>
+                </li>
+                <li className={css.header_list_item}>
+                    <a href="/movies">Фильмы</a>
+                </li>
+                <li className={css.header_list_item}>
+                    <a href="/serials">Сериалы</a>
+                </li>
+                <li className={css.header_list_item}>
+                    <a href="/search">Поиск</a>
+                </li>
             </ul>
             <div className={css.header_buttons}>
-                <div>
+                <a className={css.header_buttons_signin} href="/signin">
                     Войти
-                </div>
-                <div>
-                    Регистрация
-                </div>
+                </a>
+                <a className={css.header_buttons_signup} href="/signup">
+                    Зарегистрироваться
+                </a>
             </div>
         </div>
     );
