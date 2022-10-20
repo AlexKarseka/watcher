963111479fe3a7108830d500ab09491b2c05613b
AlexKarseka <alexkarseko@gmail.com>
2022-10-20 19:44:45 +0300

    filter english logo

--- src/components/LogoNameList.tsx
+++ src/components/LogoNameList.tsx
@@ -8,12 +8,16 @@ interface LogoNameListProps {
 
 const LogoNameList = ({logo, nameMovie}: LogoNameListProps) => {
 
+    const englishLogo = logo
+        .map(logo => logo.iso_639_1 === "en" ? {...logo, logo: logo.file_path} : logo)
+        .filter(logo => logo.iso_639_1 === "en")
+
     return (
         <div className="relative z-20 w-2/4">
             {logo.length !== 0 ?
                 <img
                     className="w-full my-4"
-                    src={`https://image.tmdb.org/t/p/w500${logo[0].file_path}`}
+                    src={`https://image.tmdb.org/t/p/w500${englishLogo[0].file_path}`}
                     alt="bg"
                 /> : null}
 
